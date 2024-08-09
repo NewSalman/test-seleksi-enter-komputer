bc43eefcb7d252498f755a4dcb7e5a27e411b77f
NewSalman <syahbani.salman@gmail.com>
2024-08-09 20:52:44 +0700

    adding account id env

--- lib/core/utils/constant.dart
+++ lib/core/utils/constant.dart
@@ -5,12 +5,14 @@
 class Constants {
 
   // api url
-  static const baseURL = "https://api.themoviedb.org/3/movie/";
+  static const baseURL = "https://api.themoviedb.org/3/";
   static const imageURL = "https://image.tmdb.org/t/p/original/";
   static const genreURL = "https://api.themoviedb.org/3/genre/movie/list?language=en";
   
   // api token
+  // never hardcoded any private or personal infomation
   static const accessToken = String.fromEnvironment("ACCESS_TOKEN");
+  static const accountId = String.fromEnvironment("ACCOUNT_ID");
 
   // local storage constants
   static const watchList = "watchList";
